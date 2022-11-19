fd0373a28e52e5a9933582de05c9c21e6205b1fe
damoo192 <damjanmladenovski2002@gmail.com>
2022-11-19 16:33:41 +0100

    lalal

--- Javascript/exercises/exercise1.1/main.js
+++ Javascript/exercises/exercise1.1/main.js
@@ -2,10 +2,24 @@
 
 //alert("My name is .....");
 
- let status = false;
+//  let status = false;
 
-  if (status){
-    console.log("Its true");
-  } else{
-    console.log("Its false");
-  }
+//   if (status){
+//     console.log("Its true");
+//   } else{
+//     console.log("Its false");
+//   }
+
+// let langth = 16;
+// let lastName = "damjan";
+
+// console.log(length);
+
+let a = 10;
+let b = 15;
+
+let c= a + b;
+
+console.log(c);
+
+console.log("The sum of " + a + " and " + b + " is: " + (a + b));
