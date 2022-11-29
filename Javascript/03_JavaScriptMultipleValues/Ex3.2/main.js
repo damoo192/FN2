c61d070fd01132d1bfb8d691554501bea8b6054a
damoo192 <damjanmladenovski2002@gmail.com>
2022-11-29 20:33:07 +0100

    Ex3.2

---
+++ Javascript/03_JavaScriptMultipleValues/Ex3.2/main.js
@@ -0,0 +1,10 @@
+
+let shopingList = ["Milk", "Bread", "Aplles"];
+shopingList.splice(1 , 1, "Banana", "Eags");
+shopingList.pop();
+shopingList.sort();
+shopingList.pop();
+shopingList.splice(1,0, "Carrots", "Lettuce");
+let shopingList2 = ["Juice", "Pop"];
+let shopingList3 = ["Juice", "Pop"];
+let TheShopingList = shopingList.concat(shopingList2,shopingList3)
