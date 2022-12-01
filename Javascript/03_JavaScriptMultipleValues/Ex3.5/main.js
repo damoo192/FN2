f8a745f13d3f7d49e0da9cd1c020ce282b954b88
damoo192 <damjanmladenovski2002@gmail.com>
2022-12-01 20:31:33 +0100

    Ex3.5

---
+++ Javascript/03_JavaScriptMultipleValues/Ex3.5/main.js
@@ -0,0 +1,21 @@
+let people = {
+    friends: [] 
+};
+
+let damjan = {
+    firstName: "Damjan",
+    lestName: "Mladenovski",
+    ID: 1
+}
+let bojan = {
+    firstName: "Bojan",
+    lestName: "Stojanovski",
+    ID: 2
+}
+let nikola= {
+    firstName: "Nikola",
+    lestName: "Angelovski",
+    ID: 3
+}
+ people.friends.push(damjan, bojan, nikola);
+ console.log (people);
